2a86fb6868311635d8fa351df0a4c8b6367dd889
hakobe <hakobe@gmail.com>
2017-11-01 22:25:13 +0900

    sort

--- src/main/java/net/douzemille/javaplayground/ch1/Ex2.java
+++ src/main/java/net/douzemille/javaplayground/ch1/Ex2.java
@@ -7,8 +7,10 @@ import java.util.Optional;
 public class Ex2 {
     public static String[] getDirectories(String path) {
         File fromDir = new File(path);
-        return Optional.ofNullable(fromDir.listFiles(File::isDirectory)).
-                map(files -> Arrays.stream(files).map(File::getName).toArray(String[]::new)).
-                orElse(new String[]{});
+        return Optional.ofNullable(
+            fromDir.listFiles(File::isDirectory)
+        ). map(files ->
+            Arrays.stream(files).map(File::getName).sorted().toArray(String[]::new)
+        ).orElse(new String[]{});
     }
 }
